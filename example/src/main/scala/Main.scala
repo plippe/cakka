f89cde8c0e13464e9cd6cc2054e520dfff996e3e
Philippe Vinchon <p.vinchon@gmail.com>
2018-10-27 16:25:53 +0200

    Update example

--- example/src/main/scala/Main.scala
+++ example/src/main/scala/Main.scala
@@ -1,26 +1,23 @@
 package com.github.plippe.cakka
 
+import cats.implicits._
 import com.amazonaws.services.lambda.AWSLambdaClientBuilder
-import scala.concurrent.Await
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration.Duration
 
 import com.github.plippe.cakka.aws._
 
 object Main extends App {
   println("Hello World")
 
-  val actor = AwsLambdaActorRef(AWSLambdaClientBuilder.defaultClient, "my-lambda")
+  val awsLambda = AWSLambdaClientBuilder.defaultClient
+  val awsLambdaFunctionName = "aws-lambda-function-name"
+  val actor = new AwsLambdaActorRef[Either[Throwable, ?]](awsLambda,
+                                                          awsLambdaFunctionName)
 
   println("TELL - START")
-  actor ! "TELL"
+  println(actor.!("TELL"))
   println("TELL - END")
 
   println("ASK - START")
-  val f = actor ? "ASK"
+  println(actor.?("ASK"))
   println("ASK - END")
-
-  println("ASK - WAITING")
-  Await.result(f, Duration.Inf)
-  println(s"ASK - $f")
 }
